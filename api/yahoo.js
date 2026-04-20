44e41c80e77f1e8afb881a31b361a6a0517305c3
Rictons <tindale4@googlemail.com>
2026-04-20 08:30:49 +0100

    Update yahoo.js

--- api/yahoo.js
+++ api/yahoo.js
@@ -1,4 +1,5 @@
 export default async function handler(req, res) {
+  // CORS headers - must be first, before anything can fail
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
